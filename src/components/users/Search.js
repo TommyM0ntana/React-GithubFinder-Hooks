98cb004101ed134632173bb5c3edf5491a148fe7
tommym0ntana <tommy.montana.harwood@gmail.com>
2020-03-27 19:14:39 +0100

    add the search functionality

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -4,11 +4,15 @@ export class Search extends Component {
   state = {
     text: ""
   };
-  //
-  onSubmit = e => e.preventDefault();
+
+  onSubmit = e => {
+    e.preventDefault();
+    this.props.searchUsers(this.state.text);
+    this.setState({ text: "" });
+  };
 
   //SET THE STA FROM THE INPUT TO THE VALUE
-  onChange = e => this.setState({ text: e.target.value });
+  onChange = e => this.setState({ [e.target.name]: e.target.value });
 
   //whit multiple items we can do {[e.target.name]}
   //
