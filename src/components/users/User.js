daafda0b656fb4bde32b94a565ae72bb69f6de2c
tommym0ntana <tommy.montana.harwood@gmail.com>
2020-03-29 18:03:22 +0200

    Add User Component UI & Layout

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,4 +1,5 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
+import { Link } from "react-router-dom";
 
 export class User extends Component {
   componentDidMount() {
@@ -8,6 +9,8 @@ export class User extends Component {
   render() {
     const {
       name,
+      company,
+      email,
       avatar_url,
       location,
       bio,
@@ -18,10 +21,61 @@ export class User extends Component {
       following,
       public_repos,
       public_gists,
+      created_at,
+      updated_at,
       hireable
     } = this.props.user;
 
-    return <div>{name}</div>;
+    return (
+      <Fragment>
+        <Link to='/' className='btn btn-light'>
+          {" "}
+          Turn Back
+        </Link>
+        Hireable: {""}
+        {hireable ? (
+          <i className='fas fa-check text-success' />
+        ) : (
+          <i className='fas fa-times-circle text-danger' />
+        )}
+        <div className='card grid-2'>
+          <div className='all-center'>
+            <img
+              src={avatar_url}
+              alt=''
+              className='round-img'
+              style={{ width: "150px" }}
+            />
+            <h2>{name}</h2>
+            <h3>Location: {location}</h3>
+          </div>
+          <div>
+            {bio && <p>{bio}</p>}
+
+            <ul>
+              <li>{email && <Fragment>{email}</Fragment>}</li>
+              <li>{location && <Fragment>{location}</Fragment>}</li>
+              <li>
+                {created_at && <Fragment>Created: {created_at}</Fragment>}
+              </li>
+              <li>
+                {updated_at && <Fragment>Updated: {updated_at}</Fragment>}
+              </li>
+            </ul>
+
+            <a href={html_url} className='btn btn-dark my-1'>
+              Go to Github Profile
+            </a>
+          </div>
+        </div>
+        <div className='card text-center'>
+          <div className='badge badge-primary'>Followers:{followers}</div>
+          <div className='badge badge-success'>Following:{following}</div>
+          <div className='badge badge-light'>Public repos:{public_repos}</div>
+          <div className='badge badge-dark'>Public Gists:{public_gists}</div>
+        </div>
+      </Fragment>
+    );
   }
 }
 
